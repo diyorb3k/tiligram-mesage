b2bee59dc7971ae71d1d46309dadb7470a4d4dc2
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-12-09 13:45:34 +0500

    dfsafsdafd

---
+++ Desktop/next/src/app/products/[productId]/page.jsx
@@ -0,0 +1,6 @@
+const Details = ({params}) => {
+  return (
+    <div>Details {params.productId}</div>
+  )
+}
+export default Details
--- Desktop/next/src/app/products/page.jsx
+++ Desktop/next/src/app/products/page.jsx
@@ -1,6 +1,14 @@
 const Products = () => {
   return (
-    <div>Products</div>
+    <div>
+        <h1>Products</h1>
+        <ul>
+            <li>Products1</li>
+            <li>Products2</li>
+            <li>Products3</li>
+            <li>Products4</li>
+        </ul>
+    </div>
   )
 }
 export default Products
