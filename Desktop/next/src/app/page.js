1e5abbb7e398346fcf0e4b263593c6b005c71acf
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-12-09 14:46:12 +0500

    fdsadfsadf

--- Desktop/next/src/app/page.js
+++ Desktop/next/src/app/page.js
@@ -1,6 +1,30 @@
+'use client'
+
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 const Hom = () => {
+  const [data,setData]=useState([])
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((response)=>{
+      setData(response.data)
+    })
+    .catch((error)=>{
+console.log('xatolik',error)
+    })
+  })
   return (
-    <div>Hom</div>
+    <div>
+    {
+      data.map((davlat) => (
+        <div key={davlat.id}>
+          <h2>{davlat.title}</h2>
+          <p>{davlat.body}</p>
+        </div>
+      ))
+    }
+  </div>
   )
 }
 export default Hom
